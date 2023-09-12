66e954e753d373b1601781bc4bb39555d208b334
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-09-11 21:47:01 -0300

    Tarefa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Tipo from '../src/sections/tipo/Tipo'
+import Tarefa from '../src/sections/tarefa/Tarefa'
 
 
 function App() {
   return (
     <div className="container">
-      <Tipo />        
+      <Tarefa />
+      {/* <Tipo />         */}
     </div>
   );
 }
---
+++ src/sections/tarefa/Tarefa.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+const Tarefa = () => {
+    // 'data', 'assunto', 'descricao', 'contato', 'tipo_id'
+    return (
+        <div>
+            <h2>Tarefa</h2>
+            <form action="">
+                <div>
+                    <label htmlFor="data">Data</label>
+                    <input type="date" id="data" name="data" />
+                </div>
+                <div>
+                    <label htmlFor="assunto">Assunto</label>
+                    <input type="text" id="assunto" name="assunto" />
+                </div>
+                <div>
+                    <label htmlFor="descricao">Descricao</label>
+                    <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label htmlFor="contato">Contato</label>
+                    <input type="text" id="contato" name="contato" />
+                </div>
+                <div>
+                    <label htmlFor="tipo_id">Tipo</label>
+                    <select name="tipo_id" id="tipo_id">
+                        <option value="" disabled>Selecione um tipo</option>
+                    </select>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Tarefa
